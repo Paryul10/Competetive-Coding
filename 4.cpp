a1cfb83ac5c7f849e7f76fb5d695c6f12fb495ad
Paryul <paryul10101010@gmail.com>
2018-10-24 00:07:26 +0530

    4added

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    long long int arr[n+1];
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        cin >> arr[i];
+    }
+    
+}
